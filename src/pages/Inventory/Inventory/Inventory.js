1b09aaa214f72ae1af8cdf2ac8bb91f4ab43be48
musahussain <mdmuslimhussain1@gmail.com>
2022-05-09 18:23:09 +0600

    made changes in inventory to decrease quantity

--- src/pages/Inventory/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory/Inventory.js
@@ -5,45 +5,47 @@ import './Inventory.css';
 const Inventory = () => {
     const {productId} = useParams();
     const [product, setProduct] = useState({});
-    const [decreasedQuantity, setDecreasedQuantity] = useState(10);
+    const [decreasedQuantity, setDecreasedQuantity] = useState(0);
 
     useEffect( () => {
         const url = `https://evening-gorge-19412.herokuapp.com/product/${productId}`;
         fetch(url)
         .then(res => res.json())
         .then(data => setProduct(data));
-
-        const quanti = parseInt(product.quantity);
-        setDecreasedQuantity(quanti);
     }, []);
     
+    useEffect( () => {
+        setDecreasedQuantity(product.quantity);
+    }, [product]);
 
     const {name, img, description, price, supplier, quantity} = product;
     
-    console.log(decreasedQuantity);
-    const decreaseQuantityChange = async(id) => {
+    
+    const decreaseQuantityChange = (id) => {
         const newQuantity = decreasedQuantity - 1;
-        await setDecreasedQuantity(newQuantity);
+        if(decreasedQuantity > 1) {
+            setDecreasedQuantity(newQuantity);
+        } 
 
-        console.log(decreasedQuantity, id);
+        console.log("decreased",decreasedQuantity, "newquantity", newQuantity, "id", id);
 
         const updatedQuantity = {
             quantity: newQuantity
         }
 
-        // const url = `https://evening-gorge-19412.herokuapp.com/inventory/${id}`;
-        // fetch(url, {
-        //     method: 'PUT',
-        //     headers: {
-        //         'content-type': 'application/json'
-        //     },
-        //     body: JSON.stringify(updatedQuantity)
-        // })
-        // .then(res => res.json())
-        // .then(data =>{
-        //     console.log('success', data);
-        //     alert('Updated successfully!');
-        // });
+        const url = `https://evening-gorge-19412.herokuapp.com/inventory/${id}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updatedQuantity)
+        })
+        .then(res => res.json())
+        .then(data =>{
+            console.log('success', data);
+            alert('Updated successfully!');
+        });
     }
 
     
@@ -56,7 +58,7 @@ const Inventory = () => {
                 <p><span>Description:</span> {description}</p>
                 <p><span>Price:</span> {price}</p>
                 <p><small><span>Supplier:</span> {supplier}</small></p>
-                <p><span>Quantity:</span> {quantity}</p>
+                <p><span>Quantity:</span> {decreasedQuantity}</p>
                 <button onClick={() => decreaseQuantityChange(productId)} style={{cursor: 'pointer', margin: '30px'}} className='btn'>Deleivered</button>
             </div>
          </div>
