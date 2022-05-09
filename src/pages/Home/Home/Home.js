1456cf77304f5f6b40f0911157fcaa6a831107c4
musahussain <mdmuslimhussain1@gmail.com>
2022-05-09 19:46:38 +0600

    added input to increase stock

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Products from '../Products/Products';
-import Inventory from '../Products/Products';
 import './Home.css';
 
 const Home = () => {
--- src/pages/Inventory/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory/Inventory.js
@@ -4,6 +4,7 @@ import './Inventory.css';
 
 const Inventory = () => {
     const {productId} = useParams();
+    const id = productId;
     const [product, setProduct] = useState({});
     const [decreasedQuantity, setDecreasedQuantity] = useState(0);
 
@@ -14,23 +15,54 @@ const Inventory = () => {
         .then(data => setProduct(data));
     }, []);
     
+
+
+
+
+
     useEffect( () => {
         setDecreasedQuantity(product.quantity);
     }, [product]);
 
-    const {name, img, description, price, supplier, quantity} = product;
-    
+    const {name, img, description, price, supplier} = product;
     
     const decreaseQuantityChange = (id) => {
         const newQuantity = decreasedQuantity - 1;
-        if(decreasedQuantity > 1) {
+        if(decreasedQuantity > 0) {
             setDecreasedQuantity(newQuantity);
+
+            const updatedQuantity = {
+                quantity: newQuantity
+            }
+    
+            const url = `https://evening-gorge-19412.herokuapp.com/inventory/${id}`;
+            fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(updatedQuantity)
+            })
+            .then(res => res.json())
+            .then(data =>{
+                console.log('success', data);
+                alert('Updated successfully!');
+            });
         } 
+        if(decreasedQuantity === 0) {
+            alert("There are no items in stock");
+        }
+        
+    }
 
-        console.log("decreased",decreasedQuantity, "newquantity", newQuantity, "id", id);
-
+    const handleQuantityIncrease = event => {
+        event.preventDefault();
+        const quantityToIncrease = event.target.increaseStock.value;
+        const convertedQuantity = parseInt(quantityToIncrease);
+        const increasedQuantity = decreasedQuantity + convertedQuantity;
+        
         const updatedQuantity = {
-            quantity: newQuantity
+            quantity: increasedQuantity
         }
 
         const url = `https://evening-gorge-19412.herokuapp.com/inventory/${id}`;
@@ -46,8 +78,8 @@ const Inventory = () => {
             console.log('success', data);
             alert('Updated successfully!');
         });
+        setDecreasedQuantity(increasedQuantity);
     }
-
     
 
     return (
@@ -60,6 +92,14 @@ const Inventory = () => {
                 <p><small><span>Supplier:</span> {supplier}</small></p>
                 <p><span>Quantity:</span> {decreasedQuantity}</p>
                 <button onClick={() => decreaseQuantityChange(productId)} style={{cursor: 'pointer', margin: '30px'}} className='btn'>Deleivered</button>
+            </div>
+
+            <div className="restock-product">
+                <h2>Restock Product :{name}</h2>
+                <form onSubmit={handleQuantityIncrease}>
+                    <input required type="number" name="increaseStock" id="increase-stock" placeholder='Enter number to restock' />
+                    <input style={{cursor: 'pointer', margin: '0 0 0 75px'}} className='btn' type="submit" value="Restock" />
+                </form>
             </div>
          </div>
     );
